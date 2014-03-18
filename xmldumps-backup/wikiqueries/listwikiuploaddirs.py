29ad11bcd200e0f36260419ee41ff0da649e958f
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-18 08:57:17 +0200

    pep8 and usage message fixup for a couple more wikiqueries scripts
    Change-Id: I30a5d878e14acee1bcafe7fb36af4094cf6052a7

--- xmldumps-backup/wikiqueries/listwikiuploaddirs.py
+++ xmldumps-backup/wikiqueries/listwikiuploaddirs.py
@@ -1,5 +1,9 @@
-import os, sys, subprocess, getopt
+import os
+import sys
+import subprocess
+import getopt
 from subprocess import Popen, PIPE
+
 
 class UploadDir(object):
 
@@ -23,26 +27,31 @@ class UploadDir(object):
         """using wmf hack... get $site and $lang and build a relative path
         out of those."""
         inputText = 'global $site, $lang; echo \"$site\t$lang\";'
-        # expect to find the runphpscriptlet script in the same directory as we are
+        # expect to find the runphpscriptlet script in dir with this script
         currentDir = os.path.dirname(os.path.realpath(__file__))
-        command = [ "python", os.path.join(currentDir,"runphpscriptletonallwikis.py"), "--scriptpath", self.scriptPath, "--scriptlet", inputText ]
+        command = ["python", os.path.join(currentDir,
+                                          "runphpscriptletonallwikis.py"),
+                   "--scriptpath", self.scriptPath, "--scriptlet", inputText]
         result = self.runCommand(command, inputText, wiki)
         if not result:
             return None, None
         if not '\t' in result:
             commandString = ' '.join(command)
-            sys.stderr.write("unexpected output from '%s' (getting site and lang for %s)\n" %(commandString, wiki))
+            sys.stderr.write("unexpected output from '%s'"
+                             "(getting site and lang for %s)\n"
+                             % (commandString, wiki))
             sys.stderr.write("output received was: %s\n" % result)
             return None, None
-        site, lang = result.split('\t',1)
-        return site,lang
+        site, lang = result.split('\t', 1)
+        return site, lang
 
     def getUploadDir(self, wiki):
         """yay, someone is running this elsewhere. they get
         the nice wgUploadDirectory value, hope that's what they
         want."""
         inputText = "global $wgUploadDirectory; echo \"$wgUploadDirectory\";"
-        command = [ "python", "runphpscriptletonallwikis.py", "--scriptpath", self.scriptPath, "--scriptlet", inputText ]
+        command = ["python", "runphpscriptletonallwikis.py", "--scriptpath",
+                   self.scriptPath, "--scriptlet", inputText]
         return self.runCommand(command, inputText, wiki)
 
     def runCommand(self, command, inputText, wiki):
@@ -55,49 +64,58 @@ class UploadDir(object):
         commandString = ' '.join(command)
         error = None
         try:
-            proc = Popen(command, stdout = PIPE, stderr = PIPE)
+            proc = Popen(command, stdout=PIPE, stderr=PIPE)
             output, error = proc.communicate()
         except:
-            sys.stderr.write("exception encountered running command %s for wiki %s with error: %s\n" % (commandString, wiki, error))
+            sys.stderr.write("exception encountered running command %s for "
+                             " wiki %s with error: %s\n" % (commandString,
+                                                            wiki, error))
             return None
         if proc.returncode or error:
-            sys.stderr.write("command %s failed with return code %s and error %s\n" % (commandString, proc.returncode,  error)) 
+            sys.stderr.write("command %s failed with return code %s and "
+                             "error %s\n" % (commandString,
+                                             proc.returncode,  error))
             return None
         if not output or not output.strip():
-            sys.stderr.write("No output from: '%s' (getting site and lang for wiki %s)\n" % (commandString, wiki)) 
+            sys.stderr.write("No output from: '%s' (getting site and lang "
+                             "for wiki %s)\n" % (commandString, wiki))
             return None
         return (output.strip())
 
-def usage(message = None):
+
+def usage(message=None):
     if message:
         sys.stderr.write(message)
-        sys.stderr.write("Usage: python listwikiuploaddirs.py --allwikis filename --scriptpath dir\n")
-        sys.stderr.write("                                   [multiversion] [closedwikis filename]\n")
-        sys.stderr.write("                                   [privatewikis filename] [wmfhack]\n")
-        sys.stderr.write("\n")
-        sys.stderr.write("This script dumps a list of media upload dirs for all specified wikis.\n")
-        sys.stderr.write("If names of closed and/or private wikis are provided, files in these lists\n")
-        sys.stderr.write("will be skipped.\n")
-        sys.stderr.write("Note that this produces an *absolute path* based on the value of\n")
-        sys.stderr.write("$wgUploadDirectory unless the 'wmfhack' option is specified, see below for that.\n")
-        sys.stderr.write("\n")
-        sys.stderr.write("--allwikis:      name of a file which contains all wikis to be processed,")
-        sys.stderr.write("                 one per line; if '-' is specified, the list will be read.\n")
-        sys.stderr.write("from stdin\n")
-        sys.stderr.write("--scriptpath:    path to MWVersion.php, if multiversion option is set,\n")
-        sys.stderr.write("                  (see 'multiversion' below), or to Maintenance.php otherwise.\n")
-        sys.stderr.write("\n")
-        sys.stderr.write("Optional arguments:\n")
-        sys.stderr.write("\n")
-        sys.stderr.write("--multiversion:  use the WMF multiversion het deployment infrastructure.\n")
-        sys.stderr.write("--closedwikis:   name of a file which contains all closed wikis (these will be\n")
-        sys.stderr.write("                 skipped even if they are included in the allwikis file.\n")
-        sys.stderr.write("--privatewikis:  name of a file which contains all private wikis (these will be\n")
-        sys.stderr.write("                 skipped even if they are included in the allwikis file.\n")
-        sys.stderr.write("--wmfhack:       use $site/$lang to put together the upload dir; works for WMF\n")
-        sys.stderr.write("                 wikis only and it is a hack so you have been warned. Note that this\n")
-        sys.stderr.write("                 produces a *path relative to the root of the WMF upload directory.*\n")
-        sys.exit(1)
+    usagemessage = """Usage: python listwikiuploaddirs.py --allwikis filename --scriptpath dir
+                                   [multiversion] [closedwikis filename]
+                                   [privatewikis filename] [wmfhack]
+
+This script dumps a list of media upload dirs for all specified wikis.
+If names of closed and/or private wikis are provided, files in these lists
+will be skipped.
+Note that this produces an *absolute path* based on the value of
+$wgUploadDirectory unless the 'wmfhack' option is specified, see below for that
+
+--allwikis:      name of a file which contains all wikis to be processed,
+                 one per line; if '-' is specified, the list will be read
+                 from stdin
+--scriptpath:    path to MWVersion.php, if multiversion option is set,
+                  (see 'multiversion' below), or to Maintenance.php otherwise
+
+Optional arguments:
+
+--multiversion:  use the WMF multiversion het deployment infrastructure
+--closedwikis:   name of a file which contains all closed wikis (these will be
+                 skipped even if they are included in the allwikis file
+--privatewikis:  name of a file which contains all private wikis (these will be
+                 skipped even if they are included in the allwikis file
+--wmfhack:       use $site/$lang to put together the upload dir; works for WMF
+                 wikis only and it is a hack so you have been warned. Note that
+                 this produces a path relative to the root of the WMF upload
+                 directory
+"""
+    sys.stderr.write(usagemessage)
+    sys.exit(1)
 
 if __name__ == "__main__":
     closedWikis = []
@@ -110,7 +128,10 @@ if __name__ == "__main__":
     allWikisFile = closedWikisFile = privateWikisFile = None
 
     try:
-        (options, remainder) = getopt.gnu_getopt(sys.argv[1:], "", [ "allwikis=", "closedwikis=", "privatewikis=", "scriptpath=", "multiversion", "wmfhack" ])
+        (options, rem) = getopt.gnu_getopt(sys.argv[1:], "",
+                                           ["allwikis=", "closedwikis=",
+                                            "privatewikis=", "scriptpath=",
+                                            "multiversion", "wmfhack"])
     except:
         usage("Unknown option specified\n")
 
@@ -128,28 +149,29 @@ if __name__ == "__main__":
         elif opt == "--wmfhack":
             wmfhack = True
 
-    if len(remainder) > 0:
+    if len(rem) > 0:
         usage("Unknown option specified\n")
 
     if not allWikisFile or not scriptPath:
         usage("One or more mandatory options is missing\n")
 
     if closedWikisFile:
-        fd = open(closedWikisFile ,"r")
-        closedWikis = [ line.strip() for line in fd ]
+        fd = open(closedWikisFile, "r")
+        closedWikis = [line.strip() for line in fd]
         fd.close()
 
     if privateWikisFile:
-        fd = open(privateWikisFile ,"r")
-        privateWikis = [ line.strip() for line in fd ]
+        fd = open(privateWikisFile, "r")
+        privateWikis = [line.strip() for line in fd]
         fd.close()
 
     if allWikisFile == "-":
         fd = sys.stdin
     else:
-        fd = open(allWikisFile ,"r")
-    wikiListTemp = [ l.strip() for l in fd ]
-    wikiList = [ l for l in wikiListTemp if l not in privateWikis and l not in closedWikis ]
+        fd = open(allWikisFile, "r")
+    wikiListTemp = [l.strip() for l in fd]
+    wikiList = [l for l in wikiListTemp if l not in privateWikis
+                and l not in closedWikis]
     if fd != sys.stdin:
         fd.close()
 
--- xmldumps-backup/wikiqueries/runphpscriptletonallwikis.py
+++ xmldumps-backup/wikiqueries/runphpscriptletonallwikis.py
@@ -1,11 +1,15 @@
-import os, sys, getopt, subprocess
+import os
+import sys
+import getopt
+import subprocess
 from subprocess import Popen, PIPE
+
 
 class PhpRunner(object):
     """Run a maintenance 'scriptlet' on all wikis
     The maintenance class framework is set up already;
-    the caller should supply a few lines of code that would go 
-    into the execute function."""
+    the caller should supply a few lines of code that would
+    go into the execute function."""
     def __init__(self, scriptPath, phpBody, multiversion, wiki):
         self.scriptPath = scriptPath
         self.phpBody = phpBody
@@ -14,17 +18,19 @@ class PhpRunner(object):
         pass
 
     def runPhpScriptlet(self):
-        command = [ "php", "--", "--wiki=%s" % self.wiki ]
+        command = ["php", "--", "--wiki=%s" % self.wiki]
         return self.runCommand(command)
 
     def runCommand(self, command):
         result = True
         try:
-            proc = Popen(command, stdin = PIPE, stdout = PIPE, stderr = PIPE)
+            proc = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
             output, error = proc.communicate(self.getPhpCode())
             if proc.returncode:
                 # don't barf, let the caller decide what to do
-                sys.stderr.write("command '%s failed with return code %s and error %s\n" % ( command, proc.returncode,  error )) 
+                sys.stderr.write("command '%s failed with return code %s and "
+                                 "error %s\n" % (command,
+                                                 proc.returncode,  error))
                 result = False
             print output
             if error:
@@ -36,9 +42,13 @@ class PhpRunner(object):
 
     def getPhpCode(self):
         if multiversion:
-            phpSetup = "require_once( '%s/MWVersion.php' ); $dir = getMediaWikiCli(''); require_once( \"$dir/maintenance/Maintenance.php\" );" % self.scriptPath
+            phpSetup = ("require_once( '%s/MWVersion.php' ); "
+                        "$dir = getMediaWikiCli(''); "
+                        "require_once( \"$dir/maintenance/Maintenance.php\" );"
+                        % self.scriptPath)
         else:
-            phpSetup = "require_once( '%s/Maintenance.php' );" % self.scriptPath
+            phpSetup = ("require_once( '%s/Maintenance.php' );"
+                        % self.scriptPath)
         return "<?php\n" + phpSetup + self.fillinScriptletTemplate()
 
     def fillinScriptletTemplate(self):
@@ -55,21 +65,32 @@ $maintClass = "MaintenanceScriptlet";
 require_once( RUN_MAINTENANCE_IF_MAIN );
 """ % self.phpBody
 
+
 def usage(message):
     if message:
         sys.stderr.write(message + '\n')
-    sys.stderr.write("Usage: runphpscriptletonallwikis.py --scriptpath path [--wikilist value] [--multiversion]\n")
-    sys.stderr.write("                 [--multiversion] [--scriptlet text] [--scriptletfile filename] [wikiname]\n")
-    sys.stderr.write("--scriptpath:    path to MWVersion.php, if multiversion option is set,\n")
-    sys.stderr.write("                 or to maintenance/Maintenance.php, otherwise\n")
-    sys.stderr.write("--wikilist:        path to list of wiki database names one per line\n")
-    sys.stderr.write("                 if filename is '-' then the list will be read from stdin\n")
-    sys.stderr.write("--multiversion:  use the WMF multiversion het deployment infrastructure\n")
-    sys.stderr.write("--scriptlet:     the php code to run, if not provided in a file\n")
-    sys.stderr.write("--scriptletfile: a filename from which to read the php code to run\n")
-    sys.stderr.write("wikiname:  name of wiki to process, if specified overrides wikilist\n")
-    sys.stderr.write("Example:\n")
-    sys.stderr.write("python getMediaInfoForAllWikis.py enwiki\n")
+    usagemessage = """Usage: runphpscriptletonallwikis.py --scriptpath path
+                 [--wikilist value] [--multiversion] [--multiversion]
+                 [--scriptlet text] [--scriptletfile filename] [wikiname]
+
+Options:
+
+--scriptpath:    path to MWVersion.php, if multiversion option is set,
+                 or to maintenance/Maintenance.php, otherwise
+--wikilist:        path to list of wiki database names one per line
+                 if filename is '-' then the list will be read from stdin
+--multiversion:  use the WMF multiversion het deployment infrastructure
+--scriptlet:     the php code to run, if not provided in a file
+--scriptletfile: a filename from which to read the php code to run
+
+Arguments:
+
+wikiname:  name of wiki to process, if specified overrides wikilist
+
+Example:
+python runphpscriptletonallwikis.py enwiki
+"""
+    sys.stderr.write(usagemessage)
     sys.exit(1)
 
 if __name__ == "__main__":
@@ -81,7 +102,10 @@ if __name__ == "__main__":
     wiki = None
 
     try:
-        (options, remainder) = getopt.gnu_getopt(sys.argv[1:], "", ["wikilist=", "multiversion", "scriptpath=", "scriptlet=", "scriptletfile="])
+        (options, remainder) = getopt.gnu_getopt(sys.argv[1:], "",
+                                                 ["wikilist=", "multiversion",
+                                                  "scriptpath=", "scriptlet=",
+                                                  "scriptletfile="])
     except:
         usage("Unknown option specified")
 
@@ -101,7 +125,7 @@ if __name__ == "__main__":
         if len(remainder) > 1 or remainder[0].startswith("--"):
             usage("Unknown option specified")
         wiki = remainder[0]
-        
+
     if (not wiki and not wikiListFile) or not scriptPath:
         usage("One of wiki or wikilist must be specified")
 
@@ -110,15 +134,15 @@ if __name__ == "__main__":
 
     if scriptlet and scriptletFile:
         usage("Only one of scriptlet or scriptletfile may be specified")
-        
+
     if wiki:
-        wikiList = [ wiki ]
+        wikiList = [wiki]
     else:
         if wikiListFile == "-":
             fd = sys.stdin
         else:
-            fd = open(wikiListFile ,"r")
-        wikiList = [ line.strip() for line in fd ]
+            fd = open(wikiListFile, "r")
+        wikiList = [line.strip() for line in fd]
 
         if fd != sys.stdin:
             fd.close()
@@ -127,8 +151,6 @@ if __name__ == "__main__":
         fd = open(scriptletFile, "r")
         scriptlet = fd.read()
         fd.close()
-
-#    phpBody = "global $wgUploadDirectory, $wgLanguageCode, $wgSitename, $wgDBname; echo \"$wgUploadDirectory $wgLanguageCode $wgSitename $wgDBname\";"
 
     fails = 0
     for w in wikiList:
