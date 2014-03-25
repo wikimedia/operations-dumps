88b1550e2077fab5977794e4e3d463ab7ab05f96
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-25 19:22:32 +0200

    generate alternate index html of dumps sorted by wiki name
    this is requested in bz 278080
    
    Change-Id: I44e91c339db5054f73a9704b1aaa1136236c3f49

--- xmldumps-backup/monitor.py
+++ xmldumps-backup/monitor.py
@@ -6,11 +6,20 @@ import WikiDump
 from os.path import exists
 from WikiDump import FileUtils
 
-def generateIndex():
+def addToFilename(filename, infix):
+    main, suffix = filename.split('.',1)
+    return main + "-" + infix + ("." + suffix if suffix else "")
+
+def generateIndex(otherIndexhtml = None, sortedByDb=False):
 	running = False
 	states = []
 	
-	for dbName in config.dbListByAge():
+        if sortedByDb:
+                dbs = sorted(config.dbList)
+        else:
+                dbs = config.dbListByAge()
+
+	for dbName in dbs:
 		wiki = WikiDump.Wiki(config, dbName)
 		if wiki.isStale():
 			print dbName + " is stale"
@@ -33,17 +42,37 @@ def generateIndex():
 	else:
 		status = "Dump process is idle."
 	
+        if otherIndexhtml is None:
+            otherIndexLink = ""
+        else:
+            if sortedByDb:
+                otherSortedBy = "dump date"
+            else:
+                otherSortedBy = "wiki name"
+
+            otherIndexLink = ('Also view sorted by <a href="%s">%s</a>'
+                              % (os.path.basename(otherIndexhtml), otherSortedBy))
+
 	return config.readTemplate("progress.html") % {
+                "otherIndexLink": otherIndexLink,
 		"status": status,
 		"items": "\n".join(states)}
 	
 def updateIndex():
 	outputFileName = os.path.join(config.publicDir, config.index)
+	outputFileNameSortedByDb = addToFilename(os.path.join(config.publicDir, config.index), "bydb")
+
 	tempFilename = outputFileName + ".tmp"
 	file = open(tempFilename, "wt")
-	file.write(generateIndex())
+	file.write(generateIndex(otherIndexhtml=outputFileNameSortedByDb))
 	file.close()
 	os.rename(tempFilename, outputFileName)
+
+	tempFilename = outputFileNameSortedByDb + ".tmp"
+	file = open(tempFilename, "wt")
+	file.write(generateIndex(otherIndexhtml=outputFileName, sortedByDb=True))
+	file.close()
+	os.rename(tempFilename, outputFileNameSortedByDb)
 
 if __name__ == "__main__":
 	# can specify name of alternate config file
