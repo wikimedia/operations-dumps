799bdc8689b59c1a02da77e13c6eff1b03bb1378
Ariel T. Glenn <ariel@wikimedia.org>
2016-03-23 00:27:45 +0200

    thumbFilesSizesCounts: full pylint and pep8
    Change-Id: I390a5d0ece37f842d93b49536086b50f39e0d161

--- tools/thumbs/crunchinglogs/datascripts/thumbFilesSizesCounts.py
+++ tools/thumbs/crunchinglogs/datascripts/thumbFilesSizesCounts.py
@@ -1,30 +1,41 @@
 # -*- coding: utf-8  -*-
 
-import os
 import re
 import sys
-import time
 import getopt
 
-def usage(message=None):
-        print "Usage: %s [--sdate=date --edate=date [filename]" % sys.argv[0]
-        print "sdate: start date for which to print stats, default: earliest date in file "
-        print "edate: end date for which to print stats, default: latest date in file"
-        print "Date format: yyyy-mm-dd"
-        print "If filename is not specified, reads from stdin"
-	print ""
-	print "Format of input file: (sample line)"
-	print "2011-10-29  01:57:51   100311   Festiwal_Słowian_i_Wikingów_2009_121.jpg/640px-Festiwal_Słowian_i_Wikingów_2009_121.jpg"
-	print "date in yyyy-mm-dd format, time in hh:mm::ss format, size in bytes, thumb directory/thumb filename"
-        sys.exit(1)
 
-if __name__ == "__main__":
+def usage(message=None):
+    if message is not None:
+        sys.stderr.write(message + "\n")
+    usage_message = """
+Usage: thumbFilesSizesCount [--sdate=date --edate=date [filename]
+
+  --sdate: start date for which to print stats, default: earliest date in file
+  --edate: end date for which to print stats, default: latest date in file
+
+Date format: yyyy-mm-dd
+
+If filename is not specified, reads from stdin
+
+Format of input file: (sample line)
+
+2011-10-29  01:57:51   100311   \
+    Festiwal_Słowian_i_Wikingów_2009_121.jpg/640px-Festiwal_Słowian_i_Wikingów_2009_121.jpg
+date in yyyy-mm-dd format, time in hh:mm::ss format, size in bytes, \
+    thumb directory/thumb filename
+"""
+    sys.stderr.write(usage_message)
+    sys.exit(1)
+
+
+def do_main():
     sdate = None
     edate = None
 
     try:
         (options, remainder) = getopt.gnu_getopt(sys.argv[1:], "",
-                                                 [ 'sdate=', 'edate=', ])
+                                                 ['sdate=', 'edate='])
     except:
         usage("Unknown option specified")
 
@@ -35,39 +46,40 @@ if __name__ == "__main__":
             edate = val
 
     dateexp = re.compile(r"^\d{4}-\d{2}-\d{2}$")
-    for d in filter(None, [ sdate, edate ]):
-        if not dateexp.match(d):
+    for date in filter(None, [sdate, edate]):
+        if not dateexp.match(date):
             usage("Bad date format.")
 
     if len(remainder) == 1:
-        inputFile = remainder[0]
-        fHandle = open(inputFile,"r")
+        input_file = remainder[0]
+        f_handle = open(input_file, "r")
     elif len(remainder) == 0:
-        fHandle = sys.stdin
+        f_handle = sys.stdin
     else:
         usage("Too many arguments.")
 
     sizes = {}
     counts = {}
-    totalSize = 0
-    totalNum = 0
-    for line in fHandle:
+    for line in f_handle:
         try:
-            ( fDate, fTime, fSize, fName ) = line.rstrip().split()
+            (f_date, ftime_unused, f_size, fname_unused) = line.rstrip().split()
         except:
             print >> sys.stderr, "skipping badly formatted line: ", line.rstrip()
             continue
-        if (sdate and (fDate >= sdate)) or not sdate:
-            if (edate and (fDate <= edate)) or not edate:
-                if not fDate in sizes:
-                    sizes[fDate] = 0
-                    counts[fDate] = 0
-                sizes[fDate] = sizes[fDate] + int(fSize)
-                counts[fDate] = counts[fDate] + 1
+        if (sdate and (f_date >= sdate)) or not sdate:
+            if (edate and (f_date <= edate)) or not edate:
+                if f_date not in sizes:
+                    sizes[f_date] = 0
+                    counts[f_date] = 0
+                sizes[f_date] = sizes[f_date] + int(f_size)
+                counts[f_date] = counts[f_date] + 1
 
     dates = sizes.keys()
     dates.sort()
-    for d in dates:
-        print "Date:", d, "Bytes:", sizes[d], "Files:", counts[d]
+    for date in dates:
+        print "Date:", date, "Bytes:", sizes[date], "Files:", counts[date]
     sys.exit(0)
 
+
+if __name__ == "__main__":
+    do_main()
