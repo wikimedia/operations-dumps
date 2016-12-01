2b35e77ac857c0eb044b8e6687bb94b70338fd60
Ariel T. Glenn <ariel@wikimedia.org>
2016-12-01 10:44:33 +0200

    make sure db_user and db_password config attributes are initialized
    Change-Id: I6e9fba888f61760f26b2d3ad4a1bf56df58c7366

--- xmldumps-backup/dumps/WikiDump.py
+++ xmldumps-backup/dumps/WikiDump.py
@@ -271,9 +271,13 @@ class Config(object):
         dbuser = self.get_opt_for_proj_or_default(conf, "database", "user", 0)
         if dbuser:
             self.db_user = dbuser
+        else:
+            self.db_user = None
         dbpassword = self.get_opt_for_proj_or_default(conf, "database", "password", 0)
         if dbpassword:
             self.db_password = dbpassword
+        else:
+            self.db_password = None
         max_allowed_packet = self.get_opt_for_proj_or_default(
             conf, "database", "max_allowed_packet", 0)
         if max_allowed_packet:
