wikimedia/operations-dumps

commit 692f02f21ffa2d36db793b4a5cfaa8f704008ebf

use sorted() everywhere instead of sort() (ptyhon 3 change)
this means we don't have to worry about whether the items
sorted later become dicts or other iterables

see https://gerrit.wikimedia.org/r/#/c/207504/ (thanks Dereckson
and John Vandenberg)

Change-Id: I2b5b149aac2017716d7c2ac29edffe7c9a3d4201

xmldumps-backup/dumps/fileutils.py
@@ -678,7 +678,7 @@ class DumpDir(object):
         """
         convert = lambda text: int(text) if text.isdigit() else text
         alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key.filename)]
-        mylist.sort(key=alphanum_key)
+        mylist = sorted(mylist, key=alphanum_key)
 
     def get_checkpt_files(self, date=None, dump_name=None,
                           file_type=None, file_ext=None, parts=False, temp=False):

xmldumps-backup/IncrDumpLib.py
@@ -334,7 +334,7 @@ class IncDumpDirs(object):
                     dates.append(dirname)
         except OSError:
             return []
-        dates.sort()
+        dates = sorted(dates)
         return dates
 
     def cleanup_old_incrdumps(self, date):

xmldumps-backup/dumpadmin.py
@@ -535,7 +535,7 @@ class ActionHandler(object):
         if self.show == 'lastrun':
             dbinfo = self.conf.db_latest_status()
             dbdates = [date for (_dbname, _status, date) in dbinfo if date is not None]
-            dbdates.sort()
+            dbdates = sorted(dbdates)
             if not len(dbdates):
                 print ""
             else:

xmldumps-backup/dumps/WikiDump.py
@@ -415,7 +415,7 @@ class Config(object):
                     print "dump dir missing status file %s?" % dump_status
             dump_failed = (status == '') or ('dump aborted' in status)
             available.append((dump_failed, date, age, dbname))
-        available.sort()
+        available = sorted(available)
         return available
 
     def read_template(self, name):
@@ -523,7 +523,7 @@ class Wiki(object):
                     dates.append(dirname)
         except OSError as ex:
             return []
-        dates.sort()
+        dates = sorted(dates)
         return dates
 
 

xmldumps-backup/dumps/utils.py
@@ -26,7 +26,7 @@ class MiscUtils(object):
             if line != "":
                 dbs.append(line)
         infile.close()
-        dbs.sort()
+        dbs = sorted(dbs)
         return dbs
 
     @staticmethod

xmldumps-backup/dumps/xmljobs.py
@@ -383,7 +383,7 @@ class XmlDump(Dump):
 
                 # get the stub list that would be used for the current run
                 stubs = self.get_stub_files(runner)
-                stubs.sort(key=lambda thing: thing.filename)
+                stubs = sorted(stubs, key=lambda thing: thing.filename)
 
                 # get the page ranges covered by stubs
                 stub_ranges = []
@@ -397,7 +397,7 @@ class XmlDump(Dump):
                 # get list of existing checkpoint files
                 chkpt_files = self.list_checkpt_files(
                     runner.dump_dir, [self.dumpname], runner.wiki.date, parts=None)
-                chkpt_files.sort(key=lambda thing: thing.filename)
+                chkpt_files = sorted(chkpt_files, key=lambda thing: thing.filename)
                 # get the page ranges covered by existing checkpoint files
                 checkpoint_ranges = [(chkptfile.first_page_id, chkptfile.last_page_id,
                                       chkptfile.partnum)
@@ -694,7 +694,7 @@ class XmlDump(Dump):
         """
         convert = lambda text: int(text) if text.isdigit() else text
         alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
-        mylist.sort(key=alphanum_key)
+        mylist = sorted(mylist, key=alphanum_key)
 
     def get_relevant_prefetch_files(self, file_list, start_page_id, end_page_id, date, runner):
         possibles = []
@@ -783,8 +783,7 @@ class XmlDump(Dump):
             dumps = [self._prefetchdate]
         else:
             dumps = self.wiki.dump_dirs()
-        dumps.sort()
-        dumps.reverse()
+        dumps = sorted(dumps, reverse=True)
         for date in dumps:
             if date == self.wiki.date:
                 runner.debug("skipping current dump for prefetch of job %s, date %s" %

commit 2c675c48c86d40b0958d1aa2e03cf328b1911cbe

fix up checkpoint file sorting
sorting of file objs was broken in 692f02f21ffa2d36db793b4a5cfaa8f704008ebf
when convertig to use sorted() everywhere

Change-Id: I3e285c01c6f08a15f2b6284506a9550fc6fd8a6b

xmldumps-backup/dumps/fileutils.py
@@ -669,7 +669,7 @@ class DumpDir(object):
                     (checkpoint and not fobj.is_checkpoint_file)):
                 continue
             files_matched.append(fobj)
-            self.sort_fileobjs(files_matched)
+            files_matched = self.sort_fileobjs(files_matched)
         return files_matched
 
     # taken from a comment by user "Toothy" on Ned Batchelder's blog (no longer on the net)
@@ -679,6 +679,7 @@ class DumpDir(object):
         convert = lambda text: int(text) if text.isdigit() else text
         alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key.filename)]
         mylist = sorted(mylist, key=alphanum_key)
+        return mylist
 
     def get_checkpt_files(self, date=None, dump_name=None,
                           file_type=None, file_ext=None, parts=False, temp=False):