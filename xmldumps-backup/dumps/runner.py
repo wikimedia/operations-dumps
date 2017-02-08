wikimedia/operations-dumps

commit c8fb9a1ac769f6a4c2908ea396f33cb72b606072

allow dumps of private tables to be skipped via config setting
Bug: T152021
Change-Id: If1a5d0c8b2fd8b57ffb434fd19904c0ed554427c

xmldumps-backup/dumps/runner.py
@@ -161,7 +161,8 @@ class DumpItemList(object):
 
             try:
                 # tables job names end in 'table' so stick that on
-                if tables_configured[table]['type'] == 'private':
+                if (tables_configured[table]['type'] == 'private' and
+                        not self.wiki.config.skip_privatetables):
                     self.dump_items.append(PrivateTable(
                         table,
                         normalize_tablejob_name(tables_configured[table]['job']),

xmldumps-backup/dumps/WikiDump.py
@@ -53,6 +53,7 @@ class Config(object):
             "mailfrom": "root@localhost",
             "smtpserver": "localhost",
             "staleage": "3600",
+            "skipprivatetables": "0",
             # "database": {
             # these are now set in get_db_user_and_password() if needed
             "user": "",
@@ -225,6 +226,7 @@ class Config(object):
         self.mail_from = self.conf.get("reporting", "mailfrom")
         self.smtp_server = self.conf.get("reporting", "smtpserver")
         self.stale_age = self.conf.getint("reporting", "staleage")
+        self.skip_privatetables = self.conf.getint("reporting", "skipprivatetables")
 
         if not self.conf.has_section('tools'):
             self.conf.add_section('tools')

commit 76e578de5090153444dcb5dbb2abac90d06033a9

fix private table dumping in dumps
Not that we use it but it would be nice if it actually worked.
This fixes a regression introduced in
c8fb9a1ac769f6a4c2908ea396f33cb72b606072

Change-Id: Ic8e03a90ba7172ab7c4e94995761f3610aec6c4d

xmldumps-backup/dumps/runner.py
@@ -161,12 +161,12 @@ class DumpItemList(object):
 
             try:
                 # tables job names end in 'table' so stick that on
-                if (tables_configured[table]['type'] == 'private' and
-                        not self.wiki.config.skip_privatetables):
-                    self.dump_items.append(PrivateTable(
-                        table,
-                        normalize_tablejob_name(tables_configured[table]['job']),
-                        tables_configured[table]['description']))
+                if tables_configured[table]['type'] == 'private':
+                    if not self.wiki.config.skip_privatetables:
+                        self.dump_items.append(PrivateTable(
+                            table,
+                            normalize_tablejob_name(tables_configured[table]['job']),
+                            tables_configured[table]['description']))
                 elif tables_configured[table]['type'] == 'public':
                     self.dump_items.append(PublicTable(
                         table,
@@ -174,7 +174,8 @@ class DumpItemList(object):
                         tables_configured[table]['description']))
                 else:
                     raise BackupError("Unknown table type in table jobs config: " +
-                                      tables_configured[table][type])
+                                      tables_configured[table]['type'] +
+                                      " for table " + table)
             except:
                 # whine about missing keys etc
                 exc_type, exc_value, exc_traceback = sys.exc_info()